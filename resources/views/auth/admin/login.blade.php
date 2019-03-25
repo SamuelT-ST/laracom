b559226b2178f886aebe911c8bd6e648a65ef60b
Samuel Trstensky <s.trstensky@gmail.com>
2019-03-25 01:07:22 +0100

    WIP

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" href="{{ asset('css/admin.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 </head>
 <body class="hold-transition skin-purple login-page">
     <div class="login-box">
